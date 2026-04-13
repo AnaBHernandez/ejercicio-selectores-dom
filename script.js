4a98cbc78a6f45001d0a19a9a241d753a927dfd0
AnaBHernandez <anahernandezfit@gmail.com>
2026-04-13 13:30:30 +0200

    feat: implementada seleccion de parrafos lipsum ejercicio 1.1

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1. Obtener todos los párrafos que están dentro del div 'lipsum'
+const todosLosParrafos = document.querySelectorAll('#lipsum p');
+
+// 2. Mostrar en la consola el primero de ellos para verificar
+console.log("El primer párrafo es:", todosLosParrafos[0]);
